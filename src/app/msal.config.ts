c8bf0f736b7c7e7011fdc28ac93d7e0acf0e3a8f
Sakthi B <sakthi_b@epam.com>
2025-07-17 19:29:05 +0530

    uri changed

--- src/app/msal.config.ts
+++ src/app/msal.config.ts
@@ -13,8 +13,8 @@ export const msalConfig: Configuration = {
   auth: {
     clientId: '9bdb2c88-630c-4314-ae11-3f57379c6b87',
     authority: 'https://login.microsoftonline.com/b41b72d0-4e9f-4c26-8a69-f949f367c91d',
-    redirectUri: 'http://localhost:4200',
-     postLogoutRedirectUri: 'http://localhost:4200',
+    redirectUri: 'https://azurefronendapplication-fhc0a0dygmgsbbam.canadacentral-01.azurewebsites.net',
+     postLogoutRedirectUri: 'https://azurefronendapplication-fhc0a0dygmgsbbam.canadacentral-01.azurewebsites.net/',
   },
   cache: {
     cacheLocation: 'localStorage',
