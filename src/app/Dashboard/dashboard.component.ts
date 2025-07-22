c1532c1257e8eb11baf11a5d2663ae2ec90cd460
Sakthi B <sakthi_b@epam.com>
2025-07-22 18:22:15 +0530

    coorected ui

--- src/app/Dashboard/dashboard.component.ts
+++ src/app/Dashboard/dashboard.component.ts
@@ -9,8 +9,7 @@ import {  HttpClient } from '@angular/common/http';
   imports: [CommonModule],
   template: `
 <div style="padding: 2rem; max-width: 800px; margin: 0 auto; text-align: center;">
-  <h2>Welcom to Dashboard</h2>
-  <h1>HI</h1>
+  <h2>Welcome to Dashboard</h2>
   <p>Hello, {{ userName }}</p>
   <p>Email: {{ userEmail }}</p>
 
