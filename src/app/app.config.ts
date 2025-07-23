80e874a585378162e1e97f63a7aa2e8fbc83e3d0
Sakthi B <sakthi_b@epam.com>
2025-07-23 12:34:12 +0530

    ssr changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,46 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {
+ MSAL_INSTANCE,
+ MSAL_GUARD_CONFIG,
+ MSAL_INTERCEPTOR_CONFIG,
+ MsalService,
+ MsalInterceptor,
+ MsalBroadcastService,
+ MsalGuard
+} from '@azure/msal-angular';
+import { InteractionType } from '@azure/msal-browser';
+import { msalInstance, msalAngularConfig } from './msal.config';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+const protectedResourceMap = new Map<string, string[]>([
+ ['https://azurepracwebapp-abgegahdakaebff7.canadacentral-01.azurewebsites.net/api',['api://f5ce4693-a450-4ba9-852d-e0db1a6b248a/access_as_user']]
+]);
+
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+ providers: [
+   provideBrowserGlobalErrorListeners(),
+   provideZoneChangeDetection({ eventCoalescing: true }),
+   provideRouter(routes), 
+   provideClientHydration(withEventReplay()),
+   provideHttpClient(withInterceptorsFromDi()),
+   
+   { provide: MSAL_INSTANCE, useValue: msalInstance },
+   { provide: MSAL_GUARD_CONFIG, useValue: msalAngularConfig },
+   {
+     provide: MSAL_INTERCEPTOR_CONFIG,
+     useValue: {
+       interactionType: InteractionType.Redirect,
+       protectedResourceMap,
+     },
+   },
+   { provide: HTTP_INTERCEPTORS, useClass: MsalInterceptor, multi: true },
+   
+   MsalService,
+   MsalGuard,
+   MsalBroadcastService
+ ]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app';
 import { appConfig } from './app/app.config';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import {
-  MsalService,
-  MsalGuard,
-  MsalInterceptor,
-  MsalBroadcastService,
-  MSAL_INSTANCE,
-  MSAL_GUARD_CONFIG,
-  MSAL_INTERCEPTOR_CONFIG,
-} from '@azure/msal-angular';
-import { InteractionType } from '@azure/msal-browser';
-import { msalInstance, msalAngularConfig } from './app/msal.config';
-
-// Define protected resources
-const protectedResourceMap = new Map<string, Array<string>>([
-  ['https://graph.microsoft.com/v1.0/me', ['user.read']],
-]);
+import { msalInstance } from './app/msal.config';
 
 msalInstance.initialize().then(() => {
-  bootstrapApplication(AppComponent, {
-    ...appConfig,
-    providers: [
-          provideRouter(routes),
-       provideHttpClient(withInterceptorsFromDi()),
-
-      { provide: MSAL_INSTANCE, useValue: msalInstance },
-      { provide: MSAL_GUARD_CONFIG, useValue: msalAngularConfig },
-      {
-        provide: MSAL_INTERCEPTOR_CONFIG,
-        useValue: {
-          interactionType: InteractionType.Redirect,
-          protectedResourceMap,
-        },
-      },
-      { provide: HTTP_INTERCEPTORS, useClass: MsalInterceptor, multi: true },
-      MsalService,
-      MsalGuard,
-      MsalBroadcastService,
-    ],
-  }).catch(err => console.error(err));
-});
+  bootstrapApplication(AppComponent, appConfig)
+    .catch(err => console.error(err));
+}).catch(err => console.error(err));
